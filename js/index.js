94273b7674a57044ef3c2bb321fc014e47b8293d
Daniel <cyubahirodaniel@gmail.com>
2025-03-10 17:30:11 +0100

    CleanUp

--- js/index.js
+++ js/index.js
@@ -2,14 +2,6 @@ const answerButton = document.querySelector('[data-js="card-button"]');
 const answerCard = document.querySelector('[data-js="card-answer"]');
 
 
-// answerButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         cardAnswer.classList.toggle('show');
-//         button.textContent = button.textContent === 'Show Answer' ? 'Hide Answer' : 'Show Answer';
-//
-//     })
-// })
-
 answerButton.addEventListener('click', () => {
     answerCard.classList.toggle('show');
     answerButton.textContent = answerButton.textContent === 'Show Answer' ? 'Hide Answer' : 'Show Answer';
