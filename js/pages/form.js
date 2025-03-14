252da82e2450c38e8b841753793ea69ba0bd2c19
Daniel <cyubahirodaniel@gmail.com>
2025-03-14 13:07:26 +0100

    Refactor the code to not use inner html

--- js/pages/form.js
+++ js/pages/form.js
@@ -5,22 +5,38 @@ const form = document.querySelector('[data-js="form"]');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  //Create card
   if (newQuestion.value.length && newAnswer.value.length) {
-    form.insertAdjacentHTML('afterend',
-        `<section class="card">
-      <p class="card__question">
-        ${newQuestion.value}
-      </p>
-      <button class="button card__button" data-js="card-button">Show Answer</button>
-      <p class="card__answer" data-js="card-answer">
-        ${newAnswer.value}
-      </p>
-      <i data-js="card-bookmark" class="card__icon icon--active" data-lucide="bookmark"></i>
-    </section>`);
+
+    const section = document.createElement('section');
+    section.classList.add('card');
+
+    const questionParagraph = document.createElement('p');
+    questionParagraph.classList.add('card__question');
+    questionParagraph.textContent = newQuestion.value;
+
+    const button = document.createElement('button');
+    button.classList.add('button', 'card__button');
+    button.setAttribute('data-js', 'card-button');
+    button.textContent = 'Show Answer';
+
+    const answerParagraph = document.createElement('p');
+    answerParagraph.classList.add('card__answer');
+    answerParagraph.setAttribute('data-js', 'card-answer');
+    answerParagraph.textContent = newAnswer.value;
+
+    const icon = document.createElement('i');
+    icon.classList.add('card__icon', 'icon--active');
+
+    section.appendChild(questionParagraph);
+    section.appendChild(button);
+    section.appendChild(answerParagraph);
+    section.appendChild(icon);
+
+    form.insertAdjacentElement('afterend', section);
+
+    //Clear the inputs for a new question
+    newQuestion.value = '';
+    newAnswer.value = '';
   }
 
-  //Clear the inputs for a new question
-  newQuestion.innerHTML = '';
-  newAnswer.innerHTML = '';
 });
